f4e9ed60a3da257b5e87483db41bcc336f9e7ae9
sahagourab1996-cell <sahagourab1996@gmail.com>
2025-09-20 20:08:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add a class to the body when the page is fully loaded to trigger a fade-in animation.
+window.addEventListener('load', () => {
+    document.body.classList.add('loaded');
+});
+
+console.log("Welcome to the Parama Jewellery Museum links page!");
